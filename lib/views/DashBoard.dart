64b9cece89610acc65ddc189e85a7c32447cae95
Muhammad Javed <91019922+javedmughal-058@users.noreply.github.com>
2022-10-26 16:40:16 +0500

    Create DashBoard.dart

---
+++ lib/views/DashBoard.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+class DashboardScreen extends StatelessWidget {
+  const DashboardScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(),
+        body: const Center(
+          child:Text('Dash Board')
+        ));
+  }
+}
