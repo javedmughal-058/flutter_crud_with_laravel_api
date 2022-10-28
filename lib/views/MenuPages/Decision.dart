7aba68cb2ae73420fec917d6f2890b27e73071bd
Muhammad Javed <91019922+javedmughal-058@users.noreply.github.com>
2022-10-28 15:09:29 +0500

    Create Decision.dart

---
+++ lib/views/MenuPages/Decision.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+class DecisionSupport extends StatelessWidget {
+  const DecisionSupport({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Decision Support'),
+    );
+  }
+}
