44121eaf5b8fd91bc07caf7e29888388476a8bfb
Muhammad Javed <91019922+javedmughal-058@users.noreply.github.com>
2022-10-28 15:09:31 +0500

    Create appraisal.dart

---
+++ lib/views/MenuPages/appraisal.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Appraisal extends StatelessWidget {
+  const Appraisal ({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Appraisal'),
+    );
+  }
+}
