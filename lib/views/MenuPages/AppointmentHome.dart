f9f598975b6f0f5b9c86e421b03328e83aeebb93
Muhammad Javed <91019922+javedmughal-058@users.noreply.github.com>
2022-10-28 15:09:47 +0500

    Create AppointmentHome.dart

---
+++ lib/views/MenuPages/AppointmentHome.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+class AppointmentHome extends StatelessWidget {
+  const AppointmentHome({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Appointment'),
+    );
+  }
+}
