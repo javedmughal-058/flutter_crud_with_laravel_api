618f37d2677ca27156055282b6b2fc9c75ea2a10
Muhammad Javed <91019922+javedmughal-058@users.noreply.github.com>
2022-10-28 15:09:37 +0500

    Create Lunch.dart

---
+++ lib/views/MenuPages/Lunch.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class LunchRequest extends StatelessWidget {
+  const LunchRequest({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Lunch'),
+    );
+  }
+}
