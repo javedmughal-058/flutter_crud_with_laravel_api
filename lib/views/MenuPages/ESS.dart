a95117e84251d80ab1c375d0feb46642349f4ba4
Muhammad Javed <91019922+javedmughal-058@users.noreply.github.com>
2022-10-28 15:09:33 +0500

    Create ESS.dart

---
+++ lib/views/MenuPages/ESS.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class EmployeeSelfService extends StatelessWidget {
+  const EmployeeSelfService({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Ess'),
+    );
+  }
+}
