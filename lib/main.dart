edb56c79805ff4846bf922ba64aa50f4acce2b38
Muhammad Javed <91019922+javedmughal-058@users.noreply.github.com>
2022-10-28 15:09:50 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_crud_with_laravel_api/controllers/UserController.dart';
+import 'package:flutter_crud_with_laravel_api/views/MenuPages/AboutUs.dart';
+import 'package:flutter_crud_with_laravel_api/views/MenuPages/AppointmentHome.dart';
+import 'package:flutter_crud_with_laravel_api/views/MenuPages/Decision.dart';
+import 'package:flutter_crud_with_laravel_api/views/MenuPages/ESS.dart';
+import 'package:flutter_crud_with_laravel_api/views/MenuPages/Garments.dart';
+import 'package:flutter_crud_with_laravel_api/views/MenuPages/Lunch.dart';
+import 'package:flutter_crud_with_laravel_api/views/MenuPages/Objective.dart';
+import 'package:flutter_crud_with_laravel_api/views/MenuPages/Policy.dart';
+import 'package:flutter_crud_with_laravel_api/views/MenuPages/Setting.dart';
+import 'package:flutter_crud_with_laravel_api/views/MenuPages/appraisal.dart';
+import 'package:flutter_crud_with_laravel_api/views/MenuPages/ctm.dart';
+import 'package:flutter_crud_with_laravel_api/views/MenuPages/leave.dart';
+import 'package:flutter_crud_with_laravel_api/views/ProfilePage.dart';
+import 'package:flutter_crud_with_laravel_api/views/USG_BOT/usg_bot.dart';
 import 'package:get/get.dart';
 import 'views/StartPage.dart';
 
@@ -19,9 +33,34 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primaryColor: Color(0xff009b9a),
+        appBarTheme: AppBarTheme(
+          elevation: 0.0,
+          backgroundColor: Color(0xff009b9a),
+        ),
       ),
       home: const StartPage(),
+      getPages: [
+        GetPage(
+            name: "/profile_page", page: () => ProfilePage(), transition: Transition.leftToRight),
+        GetPage(
+            name: "/main", page: () => MyApp(), transition: Transition.rightToLeft),
+      ],
+      routes: {
+        "/appointment_home" : (context) => AppointmentHome(),
+        "/dss_notifs" : (context) => DecisionSupport(),
+        "/ess" : (context) => EmployeeSelfService(),
+        "/leave_apply" : (context) => LeaveApply(),
+        "/garment_request" : (context) => GarmentRequest(),
+        "/lunch_request" : (context) => LunchRequest(),
+        "/ctm" : (context) => CTM(),
+        "/appraisal" : (context) => Appraisal(),
+        "/objective" : (context) => Objective(),
+        "/policies" : (context) => Policy(),
+        "/settings" : (context) => Settings(),
+        "/about_us" : (context) => AboutUS(),
+        "/usgbot" : (context) => USGBot(),
+      },
     );
   }
 }
