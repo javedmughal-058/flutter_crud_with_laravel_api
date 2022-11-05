4a67e40e037c4a99a07e2b0dde75b969ca2f42e3
Muhammad Javed <91019922+javedmughal-058@users.noreply.github.com>
2022-11-05 17:27:11 +0500

    Update UserController.dart

--- lib/controllers/UserController.dart
+++ lib/controllers/UserController.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_crud_with_laravel_api/models/MenuModel.dart';
 import 'package:get/get.dart';
 
 class UserController extends GetxController{
@@ -6,8 +9,9 @@ class UserController extends GetxController{
 
   late TextEditingController name;
   late TextEditingController email;
-
-
+  var setFav =false.obs;
+  var favorites = [].obs;
+  var favoritesmenu = <MenuModel>[].obs;
   @override
   void onInit() {
     // TODO: implement onInit
@@ -15,5 +19,28 @@ class UserController extends GetxController{
     name = TextEditingController();
     email = TextEditingController();
   }
+// Future<bool> favourite(int index)async {
+//   print("$index ${menu[index].fav}");
+//   setFav.value=!setFav.value;
+//   menu[index].fav =setFav.value;
+//   return menu[index].fav;
+//
+//
+// }
 
+  Future<void> addFavorite(MenuModel element)  async {
+    favorites.add(element.count);
+    json.encode(favorites);
+    //final SharedPreferences prefs = await SharedPreferences.getInstance();
+    //prefs.setString('favoriteSurahs', favorite);
+    favoritesmenu.add(element);
+  }
+
+  Future<void> removeFavorite(MenuModel element) async {
+    favorites.remove(element.count);
+    json.encode(favorites);
+    // final SharedPreferences prefs = await SharedPreferences.getInstance();
+    // prefs.setString('favoriteSurahs', favorite);
+    favoritesmenu.remove(element);
+  }
 }
