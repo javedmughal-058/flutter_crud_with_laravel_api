c8ecd4a5242a2205a0768f7df21469a7df4b212e
Muhammad Javed <91019922+javedmughal-058@users.noreply.github.com>
2022-11-07 15:38:09 +0500

    Create Team.dart

---
+++ lib/views/BottomPages/Team.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+class Team extends StatelessWidget {
+  const Team({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Team'),
+    );
+  }
+}
