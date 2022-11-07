ffd1627e289e7b54ea6a0a5d3c25f4739a2188ac
Muhammad Javed <91019922+javedmughal-058@users.noreply.github.com>
2022-11-07 15:38:33 +0500

    Create PR.dart

---
+++ lib/views/DSS/PR.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class PR extends StatelessWidget {
+  const PR({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('PR'),
+    );
+  }
+}
