e1823cf79ce7dc5a64a0bcbb5f02f89e85840be8
Muhammad Javed <91019922+javedmughal-058@users.noreply.github.com>
2022-11-07 15:38:37 +0500

    Create PO.dart

---
+++ lib/views/DSS/PO.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class PO extends StatelessWidget {
+  const PO({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('PO'),
+    );
+  }
+}
