256b56dc9b05c2846030ae1e2fe0ad7a2c977335
Muhammad Javed <91019922+javedmughal-058@users.noreply.github.com>
2022-11-07 09:55:56 +0500

    Save fav. menu in Shared Preference.

--- lib/controllers/UserController.dart
+++ lib/controllers/UserController.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter_crud_with_laravel_api/models/MenuModel.dart';
 import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class UserController extends GetxController{
 
@@ -30,17 +31,17 @@ class UserController extends GetxController{
 
   Future<void> addFavorite(MenuModel element)  async {
     favorites.add(element.count);
-    json.encode(favorites);
-    //final SharedPreferences prefs = await SharedPreferences.getInstance();
-    //prefs.setString('favoriteSurahs', favorite);
+    var fav = json.encode(favorites);
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setString('favoriteSurahs', fav);
     favoritesmenu.add(element);
   }
 
   Future<void> removeFavorite(MenuModel element) async {
     favorites.remove(element.count);
-    json.encode(favorites);
-    // final SharedPreferences prefs = await SharedPreferences.getInstance();
-    // prefs.setString('favoriteSurahs', favorite);
+    var fav = json.encode(favorites);
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setString('favoriteSurahs', fav);
     favoritesmenu.remove(element);
   }
 }
