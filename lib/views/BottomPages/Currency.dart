05ec4a9c114d833ba88e8c9d409f44f8a7a724cf
Muhammad Javed <91019922+javedmughal-058@users.noreply.github.com>
2022-11-07 15:38:43 +0500

    Create Currency.dart

---
+++ lib/views/BottomPages/Currency.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+class Currency extends StatelessWidget {
+  const Currency({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Currency'),
+    );
+  }
+}
