8f98d62f747972a8a07468aa714d60ba8d195d32
Muhammad Javed <91019922+javedmughal-058@users.noreply.github.com>
2022-11-07 15:38:35 +0500

    Create POC.dart

---
+++ lib/views/DSS/POC.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class POC extends StatelessWidget {
+  const POC({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('POC'),
+    );
+  }
+}
